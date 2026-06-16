4b71837748644e221b4ef81c457dbe07bb1017cd
samoush244 <samia.ketchemen@uphf.fr>
2026-06-16 21:15:17 +0200

    changement route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
-import path from "path"
+import path from "path";
+
 import authRoutes from "./routes/authroutes";
 import playerRoutes from "./routes/playerRoutes";
 import newsletterRoutes from "./routes/NewsletterRoutes";
@@ -12,19 +13,23 @@ import organizationMemberRoutes from "./routes/organizationMemberRoutes";
 import eventRoutes from "./routes/eventRoutes";
 import historyRoutes from "./routes/historyRoutes";
 import clubInfoRoutes from "./routes/clubInfoRoutes";
+import practiceCategoryRoutes from "./routes/praticeCategoryRoutes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// dossier uploads visile depuis le navigateur
+
+// dossier uploads visible depuis le navigateur
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 app.use("/upload", express.static(path.join(__dirname, "../upload")));
+
 app.get("/api/health", (_req, res) => {
   res.json({ message: "Backend OK" });
 });
-app.use ("/api/auth", authRoutes);
+
+app.use("/api/auth", authRoutes);
 app.use("/api/players", playerRoutes);
 app.use("/api/newsletter", newsletterRoutes);
 app.use("/api/contact", contactRoutes);
@@ -32,7 +37,9 @@ app.use("/api/partners", partnerRoutes);
 app.use("/api/news", newsRoutes);
 app.use("/api/teams", teamRoutes);
 app.use("/api/organization-members", organizationMemberRoutes);
-app.use("/api/events",eventRoutes);
+app.use("/api/events", eventRoutes);
 app.use("/api/histories", historyRoutes);
 app.use("/api/club-info", clubInfoRoutes);
+app.use("/api/practice-categories", practiceCategoryRoutes);
+
 export default app;
