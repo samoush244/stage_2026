9eefd4f342eb3768d15c36a7663eaa4a81e0d768
samoush244 <samia.ketchemen@uphf.fr>
2026-06-18 20:34:16 +0200

    taille1

--- club-handball-frontend/src/pages/EcoleArbitrage.tsx
+++ club-handball-frontend/src/pages/EcoleArbitrage.tsx
@@ -236,7 +236,7 @@ function EcoleArbitragePage() {
              <img
         src="/images/arbitre.jfif"
     alt="Logo école d’arbitrage"
-    className="h-14 w-14 shrink-0 rounded-full bg-white object-contain p-2 shadow-md ring-2 ring-red-600/20 md:h-16 md:w-16"
+    className="h-25 w-25 shrink-0 rounded-full bg-white object-contain p-2 shadow-md ring-2 ring-red-600/20 md:h-16 md:w-16"
   />
             <p className="mb-3 text-sm font-bold uppercase tracking-[0.25em] text-red-600">
               Organisation
