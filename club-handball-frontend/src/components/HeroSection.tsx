a6f7622f74b27b00d0e68d3bfb4ec12ef3e66394
samoush244 <samia.ketchemen@uphf.fr>
2026-06-18 21:28:17 +0200

    lie

--- club-handball-frontend/src/components/HeroSection.tsx
+++ club-handball-frontend/src/components/HeroSection.tsx
@@ -113,10 +113,10 @@ function HeroSection({ clubInfo }: HeroSectionProps) {
             */}
             <div className="mt-16 flex flex-wrap items-center justify-center gap-7 sm:flex-row">
               <Link
-                to="/événement"
+                to="/événements"
                 className="rounded-md bg-red-600 px-7 py-4 font-bold text-white transition hover:bg-red-700"
               >
-                Événement
+                Événements
               </Link>
 
               <Link
