aca7c2c0293a7ae6c06f105e7ac0af3f6374e467
samoush244 <samia.ketchemen@uphf.fr>
2026-07-01 16:01:08 +0200

    tendance

--- club-handball-frontend/src/main.tsx
+++ club-handball-frontend/src/main.tsx
@@ -4,12 +4,14 @@ import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.tsx'
 import { AuthProvider } from './context/AuthContext.tsx';
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <App />
+        <Analytics />
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
